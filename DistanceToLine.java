80ec9dafab8a7444b1454307228a420928fde8be
Roger Jaffe <rogerjaffe@gmail.com>
2014-11-13 10:22:27 -0800

    Initial commit

---
+++ DistanceToLine.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class DistanceToLine here.
+ * 
+ * @author (your names)
+ * @version (a version number or a date)
+ */
+public class DistanceToLine
+{
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param
+     */
+    public static double getDist(double A, double B, double C, double a, double b)
+    {
+
+    }
+}
---
+++ DistanceToLineTest.java
@@ -0,0 +1,70 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class DistanceToLineTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DistanceToLineTest
+{
+    /**
+     * Default constructor for test class DistanceToLineTest
+     */
+    public DistanceToLineTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testGetDist1()
+    {
+        assertEquals(2.6359, DistanceToLine.getDist(-0.3, 0.5, 1, -6.84, -3.03), 0.1);
+    }
+
+    @Test
+    public void testGetDist2()
+    {
+        assertEquals(1.9261, DistanceToLine.getDist(1.8, -0.7, 1, 2.25, 1.9), 0.1);
+    }
+
+    @Test
+    public void testGetDist3()
+    {
+        assertEquals(0, DistanceToLine.getDist(0, 0, 5, 3, 7), 0.1);
+    }
+
+    @Test
+    public void testGetDist4()
+    {
+        assertEquals(55.6255, DistanceToLine.getDist(-.2, -.7, 34.6, -9.88, -5.6), 0.1);
+    }
+}
+
+
+
+
