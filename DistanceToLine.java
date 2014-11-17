e47978e098297596afd02fd01a8006acde26391e
austinhoang <austinhoang@rocketmail.com>
2014-11-17 14:10:29 -0800

    final

--- DistanceToLine.java
+++ DistanceToLine.java
@@ -14,6 +14,14 @@ public class DistanceToLine
      */
     public static double getDist(double A, double B, double C, double a, double b)
     {
-
+     if (Math.pow(A, 2) + Math.pow(B, 2) == 0)
+     {
+      return 0.0; 
+     }
+     else 
+     {
+       double d = (Math.abs(A*a + B*b + C))/(Math.sqrt(Math.pow(A, 2) + Math.pow(B, 2)));  
+       return d;
+     }   
     }
 }
